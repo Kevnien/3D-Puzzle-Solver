fa2ff8366300ebcead3c01013b8dd67ab706c7e8
Kev <KevinN858@gmail.com>
2016-06-03 16:33:16 -0700

    On branch master  Initial commit 	new file:   bitmapNode.java 	new file:   cs560input.txt 	new file:   cs560project.java 	new file:   piece3D.java 	new file:   point3D.java 	new file:   sample.txt 	new file:   sample2.txt

---
+++ point3D.java
@@ -0,0 +1,23 @@
+// Christian Barajas, Christian Cosby, Kevin Nguyen
+// Group #15
+// CS 560
+
+//source code by Professor Bill Root, San Diego State University
+//class/file used as part of cs560project
+
+public class point3D {
+	int x;
+	int y;
+	int z;
+	
+	public point3D(){
+		
+	}
+	
+	public point3D(int ix, int iy, int iz){
+		x = ix;
+		y = iy;
+		z = iz;
+	}
+}
+
